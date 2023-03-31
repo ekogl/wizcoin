fe77e312a0b88f94867d94d1de6bcbda89e085aa
Elias Kogler <eliaskogler22@gmail.com>
2023-03-31 10:10:11 +0200

    Add balance to main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello, wizards")
+print("hello, wizards", end="\n\n")
+
+
+accounts = {
+    "Harry": 4,
+    "Hermione": 2,
+    "Ron": 1
+}
+
+for name in accounts:
+    print(f"balance for {name}: {accounts[name] * 10}")
